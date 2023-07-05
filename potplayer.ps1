0af7585f6651939d4d1b3a1da71763bb1c96301b
Xeiniac <Xeiniac@gmail.com>
2023-07-05 12:20:05 +0530

    Hide popup and mark as watched when played

--- potplayer.ps1
+++ potplayer.ps1
@@ -1,6 +1,6 @@
 Add-Type -Assembly System.Web
 $path=$args[0]
 $path=$path -replace "potplayer://" , ""
-$path= [System.Web.HttpUtility]::UrlDecode($path)
+$path= [uri]::UnescapeDataString($path)
 echo $path
 & "C:\\Program Files\\DAUM\\PotPlayer\\PotPlayerMini64.exe" $path
